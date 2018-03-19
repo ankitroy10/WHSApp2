50a266a6b8725f75930e24bdb82d825207d68bbc
Ankit Roy <ankit@Ankits-MacBook-Pro.local>
2018-03-18 20:11:55 -0400

    Initial Commit

---
+++ WHSApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WHSApp2
+//
+//  Created by Ankit Roy on 3/18/18.
+//  Copyright © 2018 Ankit Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
